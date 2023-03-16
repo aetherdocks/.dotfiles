b0223afd15ffb172f3510b233264a0c823d43841
Aether <rafa@aetherdocks.xyz>
2023-03-16 02:23:53 +0100

    made script that prints a random sonic the hedgehog quote, then made the script run when fish starts an interactive session

---
+++ bin/sonic-line.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# Prints a cheesy Sonic line
+# Gotten from this video https://www.youtube.com/embed/MCMwOL847rk
+# and this video https://www.youtube.com/embed/IeHq-xqwg3E
+#
+# from line 10, get a random line and print it
+tail --line +10 ~/.dotfiles/bin/sonic-line.sh | shuf -n 1
+
+# Line below me is line 10 - this is relevant for the script
+# All systems, full power!
+# Argh! You're not going to get away with this!
+# Get a load of this!
+# No way! I can't believe this!
+# Teryaaaaaaaaaaaaaaaaaa
+# Where's that DAMN FOURTH Chaos Emerald!
+# Oh, you mean the Chaos Emeralds?
+# Find the computer room!
+# You know what they say! The more the merrier!
+# Look at all those Eggman robots!
+# You might know everything I'm going to do, but that's not going to help you since I know everything you're going to do! Strange, isn't it?
+# Snooping as usual, I see?
+# PINGAS!
+# Come on ya big drip! Where ya goin'?
+# Because we're... Sonic heroes!
+# Even without wings, I can still fly!
+# Ah Yeah! This is happenin'!
+# All's well that ends well, right?
+# I found you, faker!
+# Faker? I think you're the fake hedgehog around here.
+# We'll show that creep the REAL SUPER POWER of TEAM WORK!
+# I'm the coolest.
+# This is WHO I AM
+# Oh no!
+# Watch out! You're gonna crash!
+# You're too slow!
+# It's no use!
+# Take this!
+# It' no use! Pull back. pull back. pull back!
+# Don't get so excited... those were the EASY ones
+# Froggy... where are you?
+# Too bad it's all over... FOR YOU!
+# Behold my floating masterpice!
+# It says, 'Cute couples get in free!'
+# THIS IS THE ULTIMATE
+# Long time no see
+# I promise you... revenge!
+# Let me show you my real power...
+# I'll show what real evil is!
+# Wow my head's spinnin'!
+# Hey I'll play with you some other time
+# I'll make you eat those words!
+# Gotta go fast
+# Enjoy your future! It's gonna be great!
+# I miss my wife Tails
+# You have 13s before the machine fucking explodes you hottopic wannabe
+# Oh yeah extra mayo
+# You're fat			lol
+# That looks very schrunchious
+# I'm going to kill you, and then kill you again
+# AND SHE HAD A DIAMOND IN HER-
+# I've come to make an announcement
+# I'm back in the fucking buildng again!
+# I'm making a callout post on my twitter.com
+# I'M PISSIN' ON THE MOON
+# HOW DO YOU LIKE THAT OBAMA?
+# YOU EDIOT
+# Everybody shu-shuddup shutup, shutup shut up SHUT UP   S H U T   U P
+# Maria... *explosion*
+# haha ha. ONE!
+# Welcome to tilted towers.
+# Welcome to my house,
+# As you can see I've knocked over many chairs because I get so tilted at the towers
+# Not many girls come in here because I get friendzoned so frequently
+# You are what you eat Sonic
+# I'd like to be in the friendzone, I like friends
+# Unfortunately, as a gamer I get no respect
+# IS THAT WHAT A HOUSE LOOKS LIKE
+# One!
+# Now I can carry around my bf wherever I desire
+# I feel like it's gonna be at least 18
+# I'm almost proud of you
+# That's the most proud of you you've ever been
+# That was an illegal left btw
+# It's my stand!
+# Top 30 reasons why Sonic is sorry. No.5 will surprise you
+# Top 30 anime deaths. No.1 is your ass right now
+# I can't see the end of the horizon-HATSUME MIKU?!
+# Something just happened.
+# I've updated my audio equipment
+# Can I have one for personal reasons?
+# If you kill someone you get all their sin points!
+# Like taking candy from a baby! Which is fine by me!
+# Yipee! I can die happy tomorrow
+# Don't just sit there and waste your precious time. Whe you wnt to do something, do it right away.
+# Do it when you can. It's the only way to live a life without regrets.
+# I will fight like I always have
+# What you see is what you get
+# I was thinking about why so many in the radical left participate in "speedrunning."
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,23 @@
 if status --is-login
 	# Adds bins from dotfiles to PATH
 	set -gx PATH $PATH ~/.dotfiles/bin
+end
 if status is-interactive
     	# Commands to run in interactive sessions can go here
+	# Abbreviations
+	abbr -a p 'sudo pacman'
+	abbr -a v 'nvim'
+	abbr -a l 'ls -la'
+	abbr -a :q 'exit' # You'll be surprised how often I did this before making it an abbreviation/alias
+	abbr -a :wq 'exit'
+
+	# Restore sudo !!
+	function pls!!
+		eval sudo $history[1]
+	end
+	# Greeting
+	function fish_greeting
+		sonic-line.sh
+	end
 end
 
